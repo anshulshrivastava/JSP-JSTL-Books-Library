1d27147d8f04e805bb1178a469b472947c97d885
anshulshrivastava <anshulandroidapp@gmail.com>
2015-05-03 12:30:47 -0500

    Update dbConnection.java

--- src/java/codes/dbConnection.java
+++ src/java/codes/dbConnection.java
@@ -15,7 +15,7 @@ public class dbConnection {
 
         try{
         Class.forName("com.mysql.jdbc.Driver");
-        con = DriverManager.getConnection("jdbc:mysql://cs2.bradley.edu:3306/books_ashrivastava2", "ashrivastava2", "mgynewuh");
+        con = DriverManager.getConnection("jdbc:mysql://cs2.bradley.edu:3306/books_ashrivastava2", "username", "password");
         }catch(Exception e){
             System.out.println("Error in Connection");
         }
